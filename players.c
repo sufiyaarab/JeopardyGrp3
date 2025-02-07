3194c97f2e99691c566b4ee4d37067140a12f184
Yara-qm <yaraqmahmoud@gmail.com>
2025-02-07 14:36:20 -0500

    Complete update score function

--- players.c
+++ players.c
@@ -34,5 +34,10 @@ bool player_exists(player *players, int num_players, char *name)
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+    for (int i = 0; i < num_players; i++){
+        if (strcmp(players[i].name, name) == 0){
+            players[i].score += score;
+            return;
+        }
+    }
 }
