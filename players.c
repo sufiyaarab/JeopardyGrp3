52662b5e153ec0f6e740b4c4cd525d73a1a49483
Yara-qm <yaraqmahmoud@gmail.com>
2025-02-07 14:35:08 -0500

    Complete player_exists function

--- players.c
+++ players.c
@@ -22,6 +22,11 @@ void initialize_players(player *players, int num_players){
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
+    for (int i = 0; i < num_players; i++){
+        if (strcmp(players[i].name, name) == 0){
+            return true;
+        }
+    }
     return false;
 }
 
