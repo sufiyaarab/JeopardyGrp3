3b2526588784b1f5b36b6091b542c398a6e89707
zainab2424 <152212878+zainab2424@users.noreply.github.com>
2025-02-10 22:51:49 -0500

    comments for players.h

--- players.h
+++ players.h
@@ -26,8 +26,7 @@ typedef struct {
 // Returns true if the player name matches one of the existing players
 extern bool player_exists(player *players, int num_players, char *name);
 
-// Go through the list of players and update the score for the 
-// player given their name
+// Go through the list of players and update the score for them 
 extern void update_score(player *players, int num_players, char *name, int score);
 
 // Initialize all of the players
